3b1eba086f9351f03f76e42526569e333f1bd786
Quan Gan <coin2028@hotmail.com>
2020-08-23 06:20:57 +0000

    oops

---
+++ 4 - large graph/utils.py
@@ -0,0 +1,34 @@
+import torch.multiprocessing as mp
+from _thread import start_new_thread
+from functools import wraps
+import traceback
+
+def prepare_mp(graph):
+    graph.in_degrees(0)
+    graph.out_degrees(0)
+    graph.find_edges([0])
+    
+def fix_openmp(func):
+    """
+    Wraps a process entry point to make it work with OpenMP.
+    """
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        queue = mp.Queue()
+        def _queue_result():
+            exception, trace, res = None, None, None
+            try:
+                res = func(*args, **kwargs)
+            except Exception as e:
+                exception = e
+                trace = traceback.format_exc()
+            queue.put((res, exception, trace))
+
+        start_new_thread(_queue_result, ())
+        result, exception, trace = queue.get()
+        if exception is None:
+            return result
+        else:
+            assert isinstance(exception, Exception)
+            raise exception.__class__(trace)
+    return decorated_function
